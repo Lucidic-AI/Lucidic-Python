fa765f394c3990fcd2436c97da5032eecc40cb2c
Abhinav Sinha <absinha@stanford.edu>
2025-05-08 12:18:22 -0700

    changed none to nonez

--- lucidicai/__init__.py
+++ lucidicai/__init__.py
@@ -158,7 +158,7 @@ def update_step(
     eval_score: Optional[float] = None,
     eval_description: Optional[str] = None,
     screenshot: Optional[str] = None,
-    screenshot_path: Optional[str] = Nonez
+    screenshot_path: Optional[str] = None
 ) -> None:
     """
     Update the current step.
