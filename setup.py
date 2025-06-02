5e4dee79bb39d33d577b52025b5a062d0f79ccf1
Abhinav Sinha <absinha@stanford.edu>
2025-06-02 10:19:28 -0700

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lucidicai",
-    version="1.1.28",
+    version="1.1.29",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
