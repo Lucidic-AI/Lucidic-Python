551c7331361a44fd264d8ee99aa1d6586df21c26
andy0liang <andy0liang@gmail.com>
2025-06-19 16:20:22 -0700

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lucidicai",
-    version="1.2.8",
+    version="1.2.9",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
