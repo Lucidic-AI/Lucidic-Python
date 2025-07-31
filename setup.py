4a2d615615e25e2e331c152774aa91e15bb56fe3
AnvitSinha <anvitsinha@gmail.com>
2025-07-30 20:33:35 -0700

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lucidicai",
-    version="1.2.19",
+    version="1.2.20",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
