d079b90d60b78f8c9e470a4b6129807ba8a03e1b
AnvitSinha <anvitsinha@gmail.com>
2025-08-27 15:36:40 -0700

    new event class

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lucidicai",
-    version="1.3.5",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
