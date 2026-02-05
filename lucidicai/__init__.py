2c7976de514dfce34294ba2637adcda5984b39f5
AnvitSinha <90011065+AnvitSinha@users.noreply.github.com>
2026-02-04 22:57:59 -0800

    Luc 398 python sdk add more thorough livekit span processing (#52)
    * update livekit integration to be more thorough
    
    * livekit test

--- lucidicai/__init__.py
+++ lucidicai/__init__.py
@@ -41,7 +41,7 @@ from .api.resources.prompt import Prompt
 from .integrations.livekit import setup_livekit
 
 # Version
-__version__ = "3.4.2"
+__version__ = "3.4.3"
 
 # All exports
 __all__ = [
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lucidicai",
-    version="3.4.2",
+    version="3.4.3",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
